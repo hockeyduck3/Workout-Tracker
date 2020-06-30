2221275c43daf98de936d28a9d75ceb4a6fc559f
LJ Spencer <hockeyduck3@gmail.com>
2020-06-29 18:29:29 -0600

    Added a total duration virtual to modal file

--- models/exercise.js
+++ models/exercise.js
@@ -2,41 +2,54 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    }, 
+const exerciseSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        }, 
 
-    exercises: [{
-        type: {
-            type: String
-        },
+        exercises: [{
+            type: {
+                type: String
+            },
 
-        name: {
-            type: String
-        },
+            name: {
+                type: String
+            },
 
-        weight: {
-            type: Number
-        },
+            weight: {
+                type: Number
+            },
 
-        distance: {
-            type: Number
-        },
+            distance: {
+                type: Number
+            },
 
-        sets: {
-            type: Number
-        },
+            sets: {
+                type: Number
+            },
 
-        reps: {
-            type: Number
-        },
+            reps: {
+                type: Number
+            },
 
-        duration: {
-            type: Number
-        },
-    }]
+            duration: {
+                type: Number
+            },
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+exerciseSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model('Workout', exerciseSchema);
